640cae49ff52d78feb3ed1984b856d4f0739618c
nomadic90 <nomadic90@gmail.com>
2016-07-10 20:16:53 +0900

    first commit

---
+++ exam_recode_play.py
@@ -0,0 +1,58 @@
+import pyaudio
+import time
+from flask import Flask, render_template, session, request
+from flask_socketio import SocketIO, emit, disconnect
+
+async_mode = None
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+
+WIDTH = 2
+CHANNELS = 2
+RATE = 48000
+
+p = pyaudio.PyAudio()
+
+@app.route('/')
+def index():
+    return render_template('index1.html', async_mode=socketio.async_mode)
+
+def callback(in_data, frame_count, time_info, status):
+    return (in_data, pyaudio.paContinue)
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('my response', {'data': 'Connected', 'count': 0})
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected', request.sid)
+
+@socketio.on('speak', namespace = '/test')
+def test_speak(stream):
+	emit('playback', {'play_stream':stream})
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
+
+
+# stream = p.open(format=p.get_format_from_width(WIDTH),
+#                 channels=CHANNELS,
+#                 rate=RATE,
+#                 input=True,
+#                 output=True,
+#                 stream_callback=callback)
+
+# stream.start_stream()
+
+# while stream.is_active():
+#     time.sleep(0.1)
+
+# stream.stop_stream()
+# stream.close()
+
+# p.terminate()
